46859e67aa5beea9238492cc31dfbdfa88d53683
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-04-13 16:21:48 +0200

    added spring boot files

---
+++ src/main/java/eu/merloteducation/aaamorchestrator/AaamOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package eu.merloteducation.aaamorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaamOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaamOrchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/merloteducation/aaamorchestrator/AaamOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.merloteducation.aaamorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaamOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
