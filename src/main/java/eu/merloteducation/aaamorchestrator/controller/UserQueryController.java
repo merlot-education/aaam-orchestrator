8b910e83448766e42ed7acb970d53359d63119dd
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-07-27 14:10:32 +0200

    streamlined exception and http response handling

--- src/main/java/eu/merloteducation/aaamorchestrator/controller/UserQueryController.java
+++ src/main/java/eu/merloteducation/aaamorchestrator/controller/UserQueryController.java
@@ -1,15 +1,20 @@
 package eu.merloteducation.aaamorchestrator.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import eu.merloteducation.aaamorchestrator.models.UserData;
 import eu.merloteducation.aaamorchestrator.views.Views;
 import eu.merloteducation.aaamorchestrator.service.KeycloakRestService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParseException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.*;
@@ -35,14 +40,12 @@ public class UserQueryController {
     /**
      * GET request, for a given organization fetch the enrolled users if allowed to do so.
      *
-     * @param orgaId   id of organization to request the users from
-     * @param response object for http code response
+     * @param orgaId id of organization to request the users from
      * @return List of user details of this organization
      */
     @GetMapping("/fromOrganization/{orgaId}")
     @JsonView(Views.UserDataView.class)
-    public List<UserData> getFromOrganization(@PathVariable(value = "orgaId") String orgaId,
-                                              HttpServletResponse response) {
+    public List<UserData> getFromOrganization(@PathVariable(value = "orgaId") String orgaId) {
         // get roles from the authenticated user
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Set<String> roles = authentication.getAuthorities()
@@ -60,19 +63,16 @@ public class UserQueryController {
         // if the requested organization id is not in the roles of this user,
         // the user is not allowed to request the list of users
         if (!orgaIds.contains(orgaId)) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            return null;
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not authorized to request users for this organization.");
         }
 
         // otherwise the user may request the user list of this organization
         try {
             return keycloakRestService.getUsersInOrganization(orgaId);
-        } catch (Exception e) {
-            System.out.println(e);
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            return null;
+        } catch (JsonProcessingException | JsonParseException | RestClientResponseException e) {
+            e.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to communicate with IAM backend.");
         }
-
     }
 
 
