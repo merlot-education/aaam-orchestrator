b4055c4660487fce47d15ebae78ba3f7e8005128
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-08-07 16:28:27 +0200

    remove unused import

--- src/main/java/eu/merloteducation/aaamorchestrator/config/CorsGlobalWebConfig.java
+++ src/main/java/eu/merloteducation/aaamorchestrator/config/CorsGlobalWebConfig.java
@@ -7,8 +7,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableWebMvc
 public class CorsGlobalWebConfig implements WebMvcConfigurer {
